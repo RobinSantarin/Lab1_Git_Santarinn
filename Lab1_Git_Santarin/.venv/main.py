b315cc8cfa6c652fd563fef950f8363739412ecb
RobinSantarin <robin.santarin@tupm.edu.ph>
2026-09-04 11:38:30 +0800

    Initial commit by Student Santarin

--- Lab1_Git_Santarin/.venv/main.py
+++
@@ -1,4 +0,0 @@
-print("Lab 1: Version Control") 
-print("Author: [Robin Santarin]") 
-print("Student ID: TUPM-26-1865")
-print("Class Section: [BSECE-1B]")
---
+++ Lab1_Git_Santarin/main.py
@@ -0,0 +1,3 @@
+print("Lab 1: Version Control") 
+print("Author: [Robin Santarin]") 
+print("Student ID: TUPM-26-1865")
